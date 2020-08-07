521ab37292868397d02efae8a5972b190ec065ec
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-07 08:04:16 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,35 @@
+// let num;
+// console.log(num);
+
+function add(num1,num2){
+    const result = num1 + num2;
+    // if you don't return your result is undefined
+}
+
+const result1 = add(5,6);
+console.log(result1);
+
+function add(num1,num2){
+    const result = num1 + num2;
+    return    //if you don't put somthing after return keyword your result is undefined
+}
+
+const result2 = add(5,6);
+console.log(result2);
+
+function add(num1,num2){
+    console.log(num2);
+}
+
+ const result3 = add(5,); //if you don't define any variable value you get undifined
+
+
+// const person ={name:'Sakib', age:22, salary:80000};
+// console.log(person.gfName);
+
+// const ages = [11, 22, 33, 9];
+// console.log(ages[9]);
+
+// All the above code are given undefined result
+
+
