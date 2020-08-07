09959b6538e2bb69002ca070b80a8eae9f9b564b
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-07 15:44:04 +0600

    break continue practice

---
+++ break-continue.js
@@ -0,0 +1,21 @@
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+for(let i = 0 ; i < numbers.length; i++){
+    if(numbers[i]>5){
+        break;
+    }
+
+    console.log(numbers[i]);
+}
+
+
+
+const numbers1 = [1,2,3,4,5,6,7,8,9];
+
+for(let i = 0 ; i < numbers1.length; i++){
+    if(numbers1[i] % 2 !== 0){
+        continue;
+    }
+
+    console.log(numbers1[i]);
+}
