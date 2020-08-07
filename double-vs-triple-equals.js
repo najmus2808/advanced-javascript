b0a24543857fc9d95b2df74c6dc3f68d7f1a22d8
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-07 08:11:31 +0600

    double vs triple equals

---
+++ double-vs-triple-equals.js
@@ -0,0 +1,35 @@
+let first = 2;
+let second = '2'
+
+if(first == second){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
+
+if(first === second){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
+
+
+
+let num1 = 1;
+let num2 = true;
+
+if(num1 == num2){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
+
+if(first === second){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
