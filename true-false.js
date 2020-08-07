391db94e8c2fe4c9cc05ce6fcb8dda77900524f7
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-07 07:16:47 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// Falsy:
+// false
+// 0
+// ""
+// NaN
+// null
+// undefined
+
+// Truthy:
+// "0"," ",[],{}
+
+
+const num =false;
+if(num){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false')
+}
