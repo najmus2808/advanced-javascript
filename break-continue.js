1c9433738ca981a1df982654bc298210cab9bf3a
najmus2808 <48199499+najmus2808@users.noreply.github.com>
2020-08-11 06:49:52 +0600

    a few correction

--- break-continue.js
+++ break-continue.js
@@ -1,21 +1,19 @@
-const numbers = [1,2,3,4,5,6,7,8,9];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for(let i = 0 ; i < numbers.length; i++){
-    if(numbers[i]>5){
-        break;
-    }
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 5) {
+    break;
+  }
 
-    console.log(numbers[i]);
+  console.log(numbers[i]);
 }
 
+const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+for (let i = 0; i < numbers1.length; i++) {
+  if (numbers1[i] % 2 !== 0) {
+    continue;
+  }
 
-const numbers1 = [1,2,3,4,5,6,7,8,9];
-
-for(let i = 0 ; i < numbers1.length; i++){
-    if(numbers1[i] % 2 !== 0){
-        continue;
-    }
-
-    console.log(numbers1[i]);
+  console.log(numbers1[i]);
 }
--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,27 +1,27 @@
 // let num;
 // console.log(num);
 
-function add(num1,num2){
+function add1(num1,num2){
     const result = num1 + num2;
     // if you don't return your result is undefined
 }
 
-const result1 = add(5,6);
+const result1 = add1(5,6);
 console.log(result1);
 
-function add(num1,num2){
+function add2(num1,num2){
     const result = num1 + num2;
     return    //if you don't put somthing after return keyword your result is undefined
 }
 
-const result2 = add(5,6);
+const result2 = add2(5,6);
 console.log(result2);
 
-function add(num1,num2){
+function add3(num1,num2){
     console.log(num2);
 }
 
- const result3 = add(5,); //if you don't define any variable value you get undifined
+ const result3 = add3(5,); //if you don't define any variable value you get undifined
 
 
 // const person ={name:'Sakib', age:22, salary:80000};
